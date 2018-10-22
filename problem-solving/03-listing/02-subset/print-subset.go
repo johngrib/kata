8f385b60f57ffb89ff3c7df257b964ebc0b35a3f
John Grib <johngrib82@gmail.com>
2018-10-22 23:36:41 +0900

    [problem solving] 3-02 print subset - add sort

--- problem-solving/03-listing/02-subset/print-subset.go
+++ problem-solving/03-listing/02-subset/print-subset.go
@@ -1,28 +1,47 @@
 package main
 
-import "fmt"
-import "sync"
+import (
+	"fmt"
+	"sort"
+)
+
+var subChan chan []int = make(chan []int)
 
 func printSubset(fixed, list []int) {
 	if len(list) == 0 {
-		fmt.Println(fixed)
+		subChan <- fixed
 		return
 	}
-	var wait sync.WaitGroup
-	wait.Add(2)
+	go printSubset(fixed, list[1:])
+	go printSubset(append(fixed, list[0]), list[1:])
+}
 
-	go func() {
-		defer wait.Done()
-		printSubset(fixed, list[1:])
-	}()
+func pow(n, m int) int {
+	if m == 1 {
+		return n
+	}
+	return n * pow(n, m-1)
+}
 
-	go func() {
-		defer wait.Done()
-		printSubset(append(fixed, list[0]), list[1:])
-	}()
-	wait.Wait()
+func runPrintSubset() {
+
+	list := []int{1, 2, 3, 4, 5}
+	size := pow(2, len(list))
+	result := [][]int{}
+
+	go printSubset([]int{}, list)
+
+	for i := 0; i < size; i++ {
+		sub := <-subChan
+		result = append(result, sub)
+	}
+
+	sort.SliceStable(result, func(a, b int) bool {
+		return len(result[a]) < len(result[b])
+	})
+	fmt.Println(result)
 }
 
 func main() {
-	printSubset([]int{}, []int{1, 2, 3, 4, 5})
+	runPrintSubset()
 }
